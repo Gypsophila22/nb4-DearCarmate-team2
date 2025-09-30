1c4d6410e0342746c1a8fd15b0629f5afb66e1d9
Jerang2 <wpckd324@gmail.com>
2025-09-30 15:50:14 +0900

    app.ts에 고객 라우터 생성

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 
 import authRouter from "./src/routers/auth.js";
+import customersRouter from "./src/routers/customers.js";
 
 dotenv.config(); // .env 파일 환경변수 적재
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 app.use("/auth", authRouter);
+app.use("/api/customers", customersRouter);
 
 app.listen(PORT, () => {
   console.log("server running");
