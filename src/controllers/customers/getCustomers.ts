d4acec2677a81fd9e287f4272af153322f5b0a12
Jerang2 <wpckd324@gmail.com>
2025-10-01 09:54:48 +0900

    getCustomers.ts에 검색 기능 추가

--- src/controllers/customers/getCustomers.ts
+++ src/controllers/customers/getCustomers.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 import type { Request, Response } from 'express';
 
 const prisma = new PrismaClient();
@@ -18,25 +18,38 @@ export const getCustomers = async (req: AuthRequest, res: Response) => {
             return res.status(401).json({ message: '인증된 사용자 정보가 없습니다.' });
         }
 
-        // 페이지네이션
+        // 페이지네이션 추가
         const page = parseInt(req.query.page as string) || 1;
         const limit = parseInt(req.query.limit as string) || 10;
-
         const skip = (page - 1) * limit;
+        const { name, email } = req.query;
+        
+        const where: Prisma.CustomersWhereInput = {
+            companyId: companyId,
+        };
+
+        if (name) {
+            where.name = {
+                contains: name as string,
+            };
+        }
+        
+        if (email) {
+            where.email = {
+                contains: email as string,
+            };
+        }
 
         const customers = await prisma.customers.findMany({
-            where: {
-                companyId: companyId,
-            },
+            where,
             take: limit,
             skip: skip,
         });
         // 전체 데이터를 조회하여 총 페이지 수 계산
         const totalCustomers = await prisma.customers.count({
-            where: {
-                companyId: companyId,
-            },
+            where,
         });
+
         const totalPages = Math.ceil(totalCustomers / limit);
         // 페이지네이션 정보를 포함하여 응답
         res.status(200).json({
