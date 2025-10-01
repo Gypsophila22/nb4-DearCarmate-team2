a89006b4fc725120db6b74f60cf743ebd6b85004
Jerang2 <wpckd324@gmail.com>
2025-10-01 10:25:04 +0900

    index.ts에 updatecustomers 추가

--- src/controllers/customers/index.ts
+++ src/controllers/customers/index.ts
@@ -1,7 +1,9 @@
 import { postCustomers} from './postCustomers.js';
 import { getCustomers } from './getCustomers.js';
+import { updateCustomers } from './updateCustomers.js';
 
 export {
     postCustomers,
-    getCustomers
+    getCustomers,
+    updateCustomers
 };
