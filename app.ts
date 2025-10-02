47427e4d0500a5a2ea10de6ac61221247c271354
shwon <kairise1234@gmail.com>
2025-10-02 11:27:10 +0900

    chore: app.ts 내 경로 수정

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import "./src/lib/passport/jwtStrategy.js";
-import passport from "passport";
-import { requestLogger } from "./src/lib/middlewares/logger.js";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import './src/lib/passport/jwtStrategy.js';
+import passport from 'passport';
+import { requestLogger } from './src/middlewares/logger.js';
 
-import authRouter from "./src/routers/auth.js";
-import userRouter from "./src/routers/users.js";
+import authRouter from './src/routers/auth.js';
+import userRouter from './src/routers/users.js';
 
-import errorHandler from "./src/middlewares/errorHandler.js";
+import errorHandler from './src/middlewares/errorHandler.js';
 
 dotenv.config(); // .env 파일 환경변수 적재
 
@@ -19,8 +19,8 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(requestLogger);
 
-app.use("/auth", authRouter);
-app.use("/users", userRouter);
+app.use('/auth', authRouter);
+app.use('/users', userRouter);
 
 app.use(errorHandler);
 
