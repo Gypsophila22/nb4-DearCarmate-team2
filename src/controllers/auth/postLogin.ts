d4914c4a527bb18f6a0a5ea81c6e1f9b066baf18
DoHyun <kyu7085@gmail.com>
2025-10-02 11:17:18 +0900

    feat: errorHandler 예시 추가

--- src/controllers/auth/postLogin.ts
+++ src/controllers/auth/postLogin.ts
@@ -1,21 +1,23 @@
-import type { Request, Response } from 'express';
-import { PrismaClient } from '../../../generated/prisma/index.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import type { NextFunction, Request, Response } from "express";
+import { PrismaClient } from "../../../generated/prisma/index.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import createError from "http-errors";
 
 const prisma = new PrismaClient();
 
-const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET || 'dev_access_secret';
-const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET || 'dev_refresh_secret';
+const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET || "dev_access_secret";
+const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET || "dev_refresh_secret";
 
 class PostLogin {
-  async login(req: Request, res: Response) {
+  async login(req: Request, res: Response, next: NextFunction) {
     try {
       const { email, password } = req.body;
 
       // 요청 유효성 검사
       if (!email || !password) {
-        return res.status(400).json({ message: '잘못된 요청입니다' });
+        // return res.status(400).json({ message: "잘못된 요청입니다" });
+        return next(createError(400, "잘못된 요청입니다."));
       }
 
       // 유저 조회
@@ -27,24 +29,26 @@ class PostLogin {
       });
 
       if (!user) {
-        return res.status(400).json({ message: '잘못된 요청입니다' });
+        // return res.status(400).json({ message: "잘못된 요청입니다" });
+        return next(createError(400, "잘못된 요청입니다."));
       }
 
       // 비밀번호 검증
       const match = await bcrypt.compare(password, user.password);
       if (!match) {
-        return res.status(400).json({ message: '잘못된 요청입니다' });
+        // return res.status(400).json({ message: "잘못된 요청입니다" });
+        return next(createError(400, "잘못된 요청입니다."));
       }
 
       // 토큰 발급
       const accessToken = jwt.sign(
         { id: user.id, email: user.email },
         ACCESS_SECRET,
-        { expiresIn: '1h' }
+        { expiresIn: "1h" }
       );
 
       const refreshToken = jwt.sign({ id: user.id }, REFRESH_SECRET, {
-        expiresIn: '7d',
+        expiresIn: "7d",
       });
 
       // 응답 (비밀번호 제외)
@@ -57,7 +61,8 @@ class PostLogin {
       });
     } catch (err) {
       console.error(err);
-      return res.status(500).json({ message: '서버 에러' });
+      // return res.status(500).json({ message: "서버 에러" });
+      return next(createError(500, "잘못된 요청입니다."));
     }
   }
 }
