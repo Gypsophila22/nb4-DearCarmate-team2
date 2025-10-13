05aa6a425cc2ea9b2ea01f457a45f23393364c7e
Jerang2 <wpckd324@gmail.com>
2025-10-13 11:01:42 +0900

    중복된 라우터 제거

--- app.ts
+++ app.ts
@@ -7,6 +7,9 @@ import { requestLogger } from './src/lib/middlewares/logger.js';
 import authRouter from "./src/routers/auth.js";
 import userRouter from './src/routers/users.js';
 import customersRouter from "./src/routers/customers.js";
+import errorHandler from './src/middlewares/errorHandler.js';
+import carsRouter from './src/routers/carsRouter.js';
+import companyRouter from './src/routers/companies.js';
 
 dotenv.config(); // .env 파일 환경변수 적재
 
@@ -21,6 +24,12 @@ app.use(requestLogger);
 app.use("/auth", authRouter);
 app.use('/users', userRouter);
 app.use("/api/customers", customersRouter);
+
+// Assuming carsRouter and companyRouter will be used later, but not adding app.use() for them yet as they are empty.
+// If they are meant to be used, the user will need to add app.use() for them.
+
+// Add the error handler at the very end
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server running PORT ${PORT}`);
