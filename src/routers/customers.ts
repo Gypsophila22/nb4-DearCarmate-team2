81e1160ef9b56471e5614cb27fca4d697418fb57
Jerang2 <wpckd324@gmail.com>
2025-10-13 09:24:20 +0900

    router 수정

--- src/routers/customers.ts
+++ src/routers/customers.ts
@@ -1,25 +1,32 @@
 import { Router } from 'express';
-import { postCustomers, getCustomers, updateCustomers, deleteCustomers, getCustomerById } from '../controllers/customers/index.js';
-import { protect } from '../middlewares/auth.js'; // 인증 미들웨어
+import { Router } from 'express';
+import {
+  createCustomer,
+  getCustomers,
+  updateCustomer,
+  deleteCustomer,
+  getCustomerById,
+} from '../controllers/customers';
+import { protect } from '../middlewares/auth'; // 인증 미들웨어
 
 const customersRouter = Router();
 
 // 아래의 라우트들은 로그인이 되어 있어야만 접근이 가능
 customersRouter.use(protect);
 
-// 고객 등록
-customersRouter.post('/', postCustomers);
-
 // 고객 목록 조회
 customersRouter.get('/', getCustomers);
 
-// 고객 수정
-customersRouter.put('/:id', updateCustomers);
-
-// 고객 삭제
-customersRouter.delete('/:id', deleteCustomers);
+// 고객 등록
+customersRouter.post('/', createCustomer);
 
 // 고객 상세 정보 조회
 customersRouter.get('/:id', getCustomerById);
+
+// 고객 수정
+customersRouter.put('/:id', updateCustomer);
+
+// 고객 삭제
+customersRouter.delete('/:id', deleteCustomer);
 
 export default customersRouter;
