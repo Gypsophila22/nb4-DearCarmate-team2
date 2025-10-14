60ba05946ea875f7077de7fdd60fd93ceca159ac
Jerang2 <wpckd324@gmail.com>
2025-10-14 12:54:18 +0900

    add customersRouter

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,10 @@
-import authRotuer from "./auth.route.js";
+import authRouter from "./auth.route.js";
 import carRouter from "./car.route.js";
 import companyRouter from "./company.route.js";
 import userRouter from "./user.route.js";
+import customersRouter from "./customer.route.js";
 
-const routers = { authRotuer, carRouter, companyRouter, userRouter };
+const routers = { authRouter, carRouter, companyRouter, userRouter, customersRouter };
 
 export default routers;
 
