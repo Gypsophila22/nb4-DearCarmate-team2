d4ae89612e7ddc339bd32c17a84afc85b6120e83
shwon <kairise1234@gmail.com>
2025-10-14 13:29:33 +0900

    chore(image): 프론트와 form-data 필드명 일치하도록 수정

--- src/routers/images.ts
+++ src/routers/images.ts
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import { upload } from '../lib/images.js';
 import { postUpload } from '../controllers/images/image.upload.js';
 
+const MULTER_IMGAEPATH = 'file';
 const router = Router();
 
-// multipart/form-data, 필드명: image
-router.post('/upload', upload.single('image'), postUpload);
+// multipart/form-data,
+router.post('/upload', upload.single(MULTER_IMGAEPATH), postUpload);
 
 export default router;
