64271c3bbbff4ce9d0ebffafdae502f7142324c1
이서진 <qps0211@naver.com>
2025-10-14 13:21:29 +0900

    fix: 불필요 주석 제거

--- src/controllers/companies/deleteCompany.ts
+++ src/controllers/companies/deleteCompany.ts
@@ -2,7 +2,6 @@ import type { Request, Response, NextFunction } from "express";
 
 async function deleteCompany(req: Request, res: Response, next: NextFunction) {
   try {
-    // TODO: Prisma delete 로직 추가 예정
     return res.json({ message: "deleteCompany not implemented yet" });
   } catch (err) {
     next(err);
