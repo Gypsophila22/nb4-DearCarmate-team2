5ba3e0be648cd109909a502da3659e25a3e5552f
Jerang2 <wpckd324@gmail.com>
2025-10-14 11:06:07 +0900

    fix: conflict marker

--- app.ts
+++ app.ts
@@ -2,24 +2,10 @@ import * as dotenv from "dotenv";
 import express from "express";
 import passport from "passport";
 
-<<<<<<< HEAD
 import errorHandler from "./src/middlewares/errorHandler.js";
 import { requestLogger } from "./src/middlewares/logger.js";
 import routers from "./src/routers/index.js";
-import customersRouter from "./src/routers/customers.js";
-=======
-import express from 'express';
-import passport from 'passport';
-
-import authRouter from "./src/routers/auth.js";
-import userRouter from './src/routers/users.js';
-import customersRouter from "./src/routers/customers.js";
-import { requestLogger } from './src/middlewares/logger.js';
-// import carsRouter from './src/routers/carsRouter.js';
-import companyRouter from './src/routers/companies.js';
-
-import errorHandler from './src/middlewares/errorHandler.js';
->>>>>>> a5f3acc (customer API 테스트 완료)
+import customersRouter from "./src/routers/customers.js"; // customer 라우터는 별도로 가져옵니다.
 
 dotenv.config(); // .env 파일 환경변수 적재
 
@@ -31,15 +17,14 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(requestLogger);
 
+// develop 브랜치의 리팩토링된 라우터들을 사용합니다.
 app.use("/auth", routers.authRouter);
 app.use("/users", routers.userRouter);
-// app.use("/cars", routers.carRouter);
+// app.use("/cars", routers.carRouter); // cars 라우터는 여전히 문제가 있으므로 주석 처리 유지
 app.use("/admin", routers.companyRouter);
+
+// customer 라우터는 별도로 추가합니다.
 app.use("/api/customers", customersRouter);
-<<<<<<< HEAD
-=======
-// app.use('/cars', carsRouter);
->>>>>>> a5f3acc (customer API 테스트 완료)
 
 app.use(errorHandler);
 
