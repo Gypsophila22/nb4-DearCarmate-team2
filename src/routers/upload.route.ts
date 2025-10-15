e05134a9e5bdd72ce73579427b9cad1003b065e0
Jerang2 <wpckd324@gmail.com>
2025-10-15 17:15:56 +0900

    /upload 추가

--- src/routers/upload.route.ts
+++ src/routers/upload.route.ts
@@ -6,7 +6,7 @@ import multer from 'multer';
 const router = Router();
 const upload = multer({ dest: 'uploads/' });
 
-router.post('/customers', upload.single('file'), uploadCustomersController);
-router.post('/cars', upload.single('file'), uploadCarsController);
+router.post('/customers/upload', upload.single('file'), uploadCustomersController);
+router.post('/cars/upload', upload.single('file'), uploadCarsController);
 
 export default router;
