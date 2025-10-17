bf83b7c9c1dfe8aeee75a73f48b454e12d341910
Gypsophila22 <kairise1234@gmail.com>
2025-10-17 11:07:12 +0900

    refactor: 리뷰 반영, 스키마 수정 (#38)
    * feat: 이미지 업로드 기능 추가(multer 사용)
    
    * feat(users, auth): 리펙터링, 테스트용 REST Client에 이미지 업로드 추가
    
    * chore(users, auth): 주석 삭제, 파일 이름 변경
    
    * feat(users, auth): cors 인증 app.ts에 추가, 프론트와 연결 가능하게 스키마 변경
    
    * chore(image): 프론트와 form-data 필드명 일치하도록 수정
    
    * feat: 이메일 발송
    
    * chore: .env.sample 수정
    
    * fix: 일반 유저 / 어드민 테스트 분리
    
    * chore: src/lib/types/express/index.d.ts 수정
    
    * fix: 유저 등록 시 users쪽에서 회사 코드 조회하도록 변경
    
    * fix: 리뷰 반영, 프론트 쪽 명세서 오류 임시 해결(미들웨어 mapCompanyKey)
    
    * fix: 스키마 변경(name, code > companyName, companyCode)
    
    * merge: 충돌 해결

--- src/users/services/user.delete.service.ts
+++ src/users/services/user.delete.service.ts
@@ -3,52 +3,15 @@ import { userDeleteRepository } from '../repositories/user.delete.repository.js'
 
 export const userDeleteService = {
   /** 본인 삭제 */
-  // async deleteMe(userId: number) {
-  //   const user = await userDeleteRepository.findById(userId);
-  //   if (!user) throw createError(404, '존재하지 않는 유저입니다.');
-
-  //   try {
-  //     await userDeleteRepository.deleteById(user.id);
-  //   } catch (e: any) {
-  //     if (e?.code === 'P2003') {
-  //       // FK 제약 등으로 삭제 불가
-  //       throw createError(409, '관련된 데이터가 있어 삭제할 수 없습니다.');
-  //     }
-  //     throw e;
-  //   }
-  //   return { message: '유저 삭제 성공.' };
-  // },
   async deleteMe(userId: number) {
-    const before = await userDeleteRepository.findById(userId);
-    console.log(
-      '[DELETE] before exists:',
-      !!before,
-      'id:',
-      userId,
-      'email:',
-      before?.email
-    );
-
-    if (!before) throw createError(404, '존재하지 않는 유저입니다.');
+    const user = await userDeleteRepository.findById(userId);
+    if (!user) throw createError(404, '존재하지 않는 유저입니다.');
 
     try {
-      const deleted = await userDeleteRepository.deleteById(before.id);
-      console.log('[DELETE] deleted row:', {
-        id: deleted.id,
-        email: deleted.email,
-      });
-
-      // ID로 재조회
-      const afterById = await userDeleteRepository.findById(before.id);
-      console.log('[DELETE] after by id is null?:', afterById === null);
-
-      // 이메일로 재조회
-      const afterByEmail = await userDeleteRepository.findByEmail(
-        deleted.email
-      );
-      console.log('[DELETE] after by email is null?:', afterByEmail === null);
+      await userDeleteRepository.deleteById(user.id);
     } catch (e: any) {
       if (e?.code === 'P2003') {
+        // FK 제약 등으로 삭제 불가
         throw createError(409, '관련된 데이터가 있어 삭제할 수 없습니다.');
       }
       throw e;
