c9636773aad6f0ad7929e75287e828b917da265c
Bogyeong Kim <bgk614@gmail.com>
2025-10-20 13:12:16 +0900

    feat: car csv upload (#46)
    * feat: car csv upload
    
    * fix: 사용 안 하는 import 삭제

---
+++ src/cars/dtos/index.ts
@@ -0,0 +1,9 @@
+import { CreateCarsRequestDto } from "./createCarsRequestDto.js";
+
+const carDto = {
+  createCarsRequest: CreateCarsRequestDto,
+};
+
+Object.freeze(carDto);
+
+export default carDto;
