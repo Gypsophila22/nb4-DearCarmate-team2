31de611bbc79ce03769ea4e63c1f28c1ddfc9628
이서진 <qps0211@naver.com>
2025-10-20 11:18:16 +0900

    fix(company) : 라우터 부분 <<<<HEAD 주석 삭제

--- src/routers/company.route.ts
+++ src/routers/company.route.ts
@@ -9,56 +9,14 @@ import companyController from "../companies/controllers/index.js";
 
 const router = express.Router();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// 회사 등록 (POST /companies)
-  router.post(
-    "/",
-    passport.authenticate("jwt", { session: false }),
-    postCompany.createCompany
-  );
-
-// 회사 목록 조회 (GET /companies)
-  router.get(
-  "/companies",
-    passport.authenticate("jwt", { session: false }),
-  companyController.getCompany
-  );
-
-// 회사별 유저 조회 (GET /companies/:companyId/users)
-router.get(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  async (req: Request, res: Response, next: NextFunction) => {
-    console.warn("테스트용 더미 코드");
-    res.json({
-      currentPage: 1,
-      totalPages: 1,
-      totalItemCount: 1,
-      data: [
-        {
-          id: 1,
-          companyName: "string",
-          companyCode: "string",
-          userCount: 3,
-        },
-      ],
-    });
-  }
-=======
-=======
-
->>>>>>> 037d69e (develop 최신화 && users 파트 companyCode, companyName merge 전 임시 변경)
 //console.log("companyController 확인:", companyController);
-
 
 // 회사 등록 (POST /companies)1
 router.post(
   "/",
-  passport.authenticate("jwt", { session: false }),
+   passport.authenticate("jwt", { session: false }),
    companyController.createCompany
 );
-
 
 // 회사 목록 조회 (GET /companies)
 router.get(
@@ -67,15 +25,12 @@ router.get(
    companyController.getCompany
 );
 
-
 // 회사별 유저 조회 (GET /companies/:companyId/users)
 router.get(
-  "/:company/users",
+  "/:companyId/users",
   passport.authenticate("jwt", { session: false }),
    companyController.getCompanyUsers
->>>>>>> b891420 (test,feat,fix(company) : 테스트를 위한 추가 http 파일 추가 및 일부 주석 삭제, 확인용 console 라인 삭제)
 );
-
 
 // 회사 수정 (PATCH /companies/:companyId)
 router.patch(
@@ -84,7 +39,6 @@ router.patch(
   companyController.updateCompany
 );
 
-
 // 회사 삭제 (DELETE /companies/:companyId)
 router.delete(
   "/:companyId",
@@ -92,14 +46,4 @@ router.delete(
   companyController.deleteCompany
 );
 
-// 회사 삭제 (DELETE /companies/:companyId)
-router.delete(
-  "/:companyId",
-  passport.authenticate("jwt", { session: false }),
-  companyController.deleteCompany
-)
-
 export default router;
-
-
-
