0bcfbcacda4a79cbff35b2404623df2953afa115
shwon <kairise1234@gmail.com>
2025-10-21 12:48:22 +0900

    chore: 중복 파일 삭제

--- src/auth/config/token.ts
+++
@@ -1,4 +0,0 @@
-export const TOKEN = {
-  access: { secret: process.env.ACCESS_TOKEN_SECRET!, expiresIn: '1h' },
-  refresh: { secret: process.env.REFRESH_TOKEN_SECRET!, expiresIn: '7d' },
-} as const;
