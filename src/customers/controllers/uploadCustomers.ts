8c1b254fce36a9fa0f387cecb59e73d0301f08b0
Jerang2 <wpckd324@gmail.com>
2025-10-22 09:33:19 +0900

    fix: 고객 대용량 업로드 에러 수정, cascade, transaction 추가 (#64)
    * 파라미터 프론트엔드와 일치하게 수정
    
    * as 제거
    
    * passport로 인증 받기, auth.ts 삭제
    
    * companyId 추가
    
    * prettier conflict
    
    * migrate 재설정, csvfile -> file
    
    * add cascade
    
    * add transaction
    
    * 한글명, as any타입 수정
    
    * 컨트롤러내 영문변환
    
    * 쌍따옴표 -> 홑따옴표

--- src/customers/controllers/uploadCustomers.ts
+++ src/customers/controllers/uploadCustomers.ts
@@ -11,19 +11,19 @@ const upload = multer({ storage: multer.memoryStorage() });
 export { upload };
 
 const customerCsvRowSchema = z.object({
-  고객명: z.string().min(1, '고객명은 필수입니다.'),
-  성별: z.enum(['MALE', 'FEMALE'], {
-    message: '성별은 MALE 또는 FEMALE이어야 합니다.',
+  customerName: z.string().min(1, '고객명은 필수입니다.'),
+  gender: z.enum(['male', 'female'], {
+    message: '성별은 male 또는 female이어야 합니다.',
   }),
-  연락처: z
+  contact: z
     .string()
     .regex(
       /^\d{2,4}-\d{3,4}-\d{4}$|^\d{9,11}$/,
       '유효한 연락처 형식이 아닙니다.',
     ),
-  연령대: z.string().optional(), // '20대', '30대' 등등
-  이메일: z.string().email('유효한 이메일 형식이 아닙니다.').optional(),
-  메모: z.string().optional(),
+  ageGroup: z.string().optional(), // '20대', '30대' 등등
+  email: z.string().email('유효한 이메일 형식이 아닙니다.').optional(),
+  memo: z.string().optional(),
 });
 
 // CSV 파싱 및 유효성 검사
