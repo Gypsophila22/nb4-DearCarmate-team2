630d361c8719bbb727a9ff73433119fe6cf1aace
Bogyeong Kim <bgk614@gmail.com>
2025-10-23 09:35:38 +0900

    feat: csv 업로드 트랜잭션 및 차량 번호 중복으로 안 들어가게 처리, 차량 cascade 추가 (#78)
    * feat: car csv upload
    
    * fix: 사용 안 하는 import 삭제
    
    * fix: 빌드에러
    
    * merge: develop 브랜치 반영
    
    * merge: develop 브랜치 반영
    
    * feat: cascade 추가
    
    차량 삭제시 계약 함께 삭제
    
    * fix: 차량 번호 중복 불가능하게 수정
    
    * feat: 차량 csv 업로드 트랜잭션
    
    * merge: develop 브랜치 반영
    
    * merge: develop 브랜치 반영

--- src/cars/repositories/index.ts
+++ src/cars/repositories/index.ts
@@ -1,13 +1,21 @@
-import { carCreateManyRepository } from './car.create-many.repository.js';
-import { createCarsModelRepository } from './createCarModelsRepository.js';
+import { carFindManyModelRepository } from './car.find-many-model.repository.js';
+import { createManyTx } from './car.create-many.repository.js';
 import { findCarModelRepository } from './findCarModelRepository.js';
+import { findManyByCarNumbers } from './cat.find-many-by-car-number.repository.js';
 import { getCarsListRepository } from './getList.js';
+import {
+  createCarsModelRepository,
+  createCarsModelTxRepository,
+} from './createCarModelsRepository.js';
 
 const carRepository = {
   getList: getCarsListRepository,
-  createMany: carCreateManyRepository,
+  createManyTx: createManyTx,
   findModel: findCarModelRepository,
   createModel: createCarsModelRepository,
+  findManyModel: carFindManyModelRepository,
+  createModelTx: createCarsModelTxRepository,
+  findManyByCarNumbers: findManyByCarNumbers,
 };
 
 Object.freeze(carRepository);
